565cc4a6dc4f0802c55b062e106c2c2aa1345a64
Squall Leonhart <squallpl1983@gmail.com>
2021-02-25 20:02:21 -0500

    Basic skeletal layout for the Overview component drafted.
    Roughly matching the adobe mock-up.

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -9,7 +9,42 @@ class Overview extends React.Component {
   render() {
     return(
       <div id="overview">
-        <p>Hello World!</p>
+        <div id="product-display">
+          <div id="image-gallery"></div>
+          <div id="product-details">
+            <div id="star-rating">Start Rating = *****</div>
+            <p id="category">Category</p>
+            <h3 id="product-name">Ugly Christmas Sweater</h3>
+            <p id="product-price">Price</p>
+            <div id="product-styles">
+              <p className="style-header">Style > <strong>Current Style</strong></p>
+              <div id="styles-container">
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+                <div className="style-bubble"></div>
+              </div>
+            </div>
+            <div id="purchase-options">
+              <div className="outline">
+                <div id="size-selector" className="purchase-buttons">Size</div>
+                <div id="quantity-selector" className="purchase-buttons">Quant</div>
+              </div>
+              <div>
+                <div id="add-to-cart-button" className="purchase-buttons">Add To Cart</div>
+                <div id="favorite" className="purchase-buttons">*</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="product-description" className="outline">
+          <h3>Product Description</h3>
+          <p>Ea quis esse amet laborum dolore nisi labore duis do. Proident aliqua commodo aliqua officia cupidatat duis aliquip amet commodo aliquip ea eu officia dolore. Anim reprehenderit ut amet dolor cillum aliquip adipisicing nulla.</p>
+        </div>
       </div>
     );
   }
--- example_config.js
+++
@@ -1,3 +0,0 @@
-var TOKEN = '82c946ea123480d0f739c174013d2774edeae1ba';
-
-module.exports = { TOKEN };
