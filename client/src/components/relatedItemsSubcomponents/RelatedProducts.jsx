77babf46cf291d2236811206acd89a7716f69d22
fliszt2 <rkelly2012@gmail.com>
2021-03-01 14:51:27 -0500

    Trying but failing to get this carousel thing happening

--- client/src/components/relatedItemsSubcomponents/RelatedProducts.jsx
+++ client/src/components/relatedItemsSubcomponents/RelatedProducts.jsx
@@ -4,52 +4,8 @@ import ProductCard from './ProductCard.jsx';
 var RelatedProducts = ({ relatedProductIds }) => (
   <div className='related-products'>
     {relatedProductIds.map(relatedProductId => <ProductCard relatedProductId={relatedProductId} key={relatedProductId} />)}
+    <span id="more-button">More</span>
   </div>
 );
 
 export default RelatedProducts;
-
-// class RelatedProducts extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       relatedProductIds: this.props.relatedProductIds
-//     };
-//   }
-
-//   // componentDidUpdate(prevProps) {
-//   //   if (prevProps !== this.props) {
-//   //     console.log('resetting state for RelatedProducts!');
-//   //     console.log('this.state.relatedProductIds:', this.state.relatedProductIds);
-//   //     this.setState({
-//   //       relatedProducts: this.props.relatedProducts
-//   //     });
-//   //   }
-//   // }
-
-//   render() {
-//     var { relatedProductIds } = this.props;
-//     return (
-//       <div>
-//         {this.props.relatedProductIds.map(relatedProductId => {
-//           return (
-//             <ProductCard relatedProductId={relatedProductId} key={relatedProductId}/>
-//           );
-//         })}
-//       </div>
-//     );
-//   }
-// }
-
-    {/* return (
-      <div>
-        <h3>Related Products Carousel</h3>
-        {this.state.relatedProducts.map(product => {
-          console.log('product:', product);
-          return (
-            <ProductCard product={product} productObject={product.productObject} stylesObject={product.stylesObject} key={product.id} />
-          );
-        })}
-      </div>
-    ); */}
