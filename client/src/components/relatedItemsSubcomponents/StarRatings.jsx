14ede33d2b80a063b420058d53b5a3a5428cf277
fliszt2 <rkelly2012@gmail.com>
2021-03-02 16:01:34 -0500

    Dynamically rendering stars for review number

--- client/src/components/relatedItemsSubcomponents/StarRatings.jsx
+++ client/src/components/relatedItemsSubcomponents/StarRatings.jsx
@@ -9,9 +9,10 @@ const halfStarElement = <FontAwesomeIcon icon={faStarHalf} />
 class StarRatings extends React.Component {
   constructor(props) {
     super(props);
+    this.renderFullStar = this.renderFullStar.bind(this);
     this.state = {
       relatedProductId: this.props.relatedProductId,
-      starRating: 0
+      starRating: 5
     };
   }
 
@@ -26,22 +27,57 @@ class StarRatings extends React.Component {
       var averageRating = scoresTotal / reviewsTotal;
       var starRating = ((Math.round(averageRating * 4)) / 4).toFixed(2);
       this.setState({
-        starRating: starRating
+        starRating: Number(starRating)
       });
     });
   }
 
+  renderFullStar(num) {
+    var starString = [];
+    for (var i = 0; i < num; i++) {
+      starString.push(<span key={i} className="star-icon">{fullStarElement}</span>);
+    }
+    return (starString);
+  }
+
   render() {
-    return (
-      <div className='star-container'>
-        <span>&nbsp;&nbsp;</span>
-        <span className="star-icon">{fullStarElement}</span>
-        <span className="star-icon">{fullStarElement}</span>
-        <span className="star-icon">{fullStarElement}</span>
-        <span className="star-icon">{fullStarElement}</span>
-        <span className="star-icon">{halfStarElement}</span>
-      </div>
-    );
+    if (this.state.starRating === 5) {
+      return (
+        <div className='star-container'>
+          <span>&nbsp;&nbsp;</span>
+          <span className="star-icon"></span>
+          <span className="star-icon"></span>
+          <span className="star-icon"></span>
+          <span className="star-icon"></span>
+          <span className="star-icon"></span>
+        </div>
+      );
+    } else if (this.state.starRating === 3.25 || this.state.starRating === 3.5 || this.state.starRating === 3.75) {
+      return (
+        <div className='star-container'>
+          <span>&nbsp;&nbsp;</span>
+          {this.renderFullStar(3)}
+          <span className="star-icon">{halfStarElement}</span>
+          <span className="star-icon"></span>
+        </div>
+      );
+    } else if (this.state.starRating === 3) {
+      return (
+        <div className='star-container'>
+          <span>&nbsp;&nbsp;</span>
+          {this.renderFullStar(3)}
+          <span className="star-icon"></span>
+          <span className="star-icon"></span>
+        </div>
+      );
+    } else {
+      return (
+        <div className='star-container'>
+          <span>&nbsp;&nbsp;</span>
+          {this.renderFullStar(5)}
+        </div>
+      )
+    }
   };
 
 }
