6ce8108a766320a8e675be7d795b4494c90713ea
fliszt2 <rkelly2012@gmail.com>
2021-03-04 11:21:05 -0500

    Persistent session cookie attached

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const path = require('path');
 const config = require('../config.js');
+// var cookieParser = require('cookie-parser');
+var session = require('express-session');
 
 const PORT = 3000;
 const app = express();
@@ -11,6 +13,14 @@ const api = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe';
 
 app.use(express.static(path.join(__dirname, '..', '/client/dist')));
 app.use(bodyParser.json());
+// app.use(cookieParser());
+var farFuture = new Date(new Date().getTime() + (1000*60*60*24*365*10));
+app.use(session({
+  secret: "Shh, its a secret!",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { expires: farFuture }
+}));
 
 app.get('/', (req, res) => {
   console.log('hi from app.get');
@@ -19,6 +29,12 @@ app.get('/', (req, res) => {
 
 app.get('/get', (req, res) => {
   var endPoint = req.query.path;
+  if (req.session.user_id) {
+    console.log('already existant req.session');
+  } else {
+    req.session.user_id = 'unique new york';
+    console.log('req.session:', req.session);
+  }
   axios.get(api + '/' + endPoint, {
     headers: {
       'Authorization': config.TOKEN
