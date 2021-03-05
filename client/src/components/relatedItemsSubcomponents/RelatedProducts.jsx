b26e8b8802e46b0ee83ba49d039f52a5ad4da0d8
fliszt2 <rkelly2012@gmail.com>
2021-03-05 11:32:49 -0500

    Adding a next-product button. point of not return re: css styles

--- client/src/components/relatedItemsSubcomponents/RelatedProducts.jsx
+++ client/src/components/relatedItemsSubcomponents/RelatedProducts.jsx
@@ -1,19 +1,72 @@
 import React from 'react';
 import ProductCard from './ProductCard.jsx';
 
-var RelatedProducts = ({ relatedProductIds, currentProductFeatures, currentProductName }) => {
-  if (relatedProductIds.length > 0) {
+class RelatedProducts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleNextProductClick = this.handleNextProductClick.bind(this);
+    this.state = {
+      allProducts: [],
+      shownProducts: [],
+      hiddenProducts: []
+    }
+  }
+
+  handleNextProductClick() {
+    console.log('trying to go right');
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.relatedProductIds !== prevProps.relatedProductIds) {
+      this.setState({
+        allProducts: this.props.relatedProductIds,
+        shownProducts: this.props.relatedProductIds.slice(0, 4),
+        hiddenProducts: this.props.relatedProductIds.slice(4)
+      });
+    }
+  }
+
+  render() {
+    var { currentProductFeatures, currentProductName } = this.props;
+
+    if (this.state.allProducts.length === 0) {
+      return (
+        <div className='related-products'>
+        </div>
+      );
+    }
+
     return (
       <div className='related-products'>
-        {relatedProductIds.map(relatedProductId => <ProductCard isRelatedProduct={true} relatedProductId={relatedProductId} key={relatedProductId} currentProductFeatures={currentProductFeatures} currentProductName={currentProductName} />)}
-      </div>
-    );
-  } else {
-    return (
-      <div className='related-products'>
+        {this.state.shownProducts.map(relatedProductId => <ProductCard isShown={true} isRelatedProduct={true} relatedProductId={relatedProductId} key={relatedProductId} currentProductFeatures={currentProductFeatures} currentProductName={currentProductName} />)}
+        <button type='button' className='next-product-button' onClick={this.handleNextProductClick}>&#10006;</button>
+        {this.state.hiddenProducts.map(relatedProductId => <ProductCard isShown={false} isRelatedProduct={true} relatedProductId={relatedProductId} key={relatedProductId} currentProductFeatures={currentProductFeatures} currentProductName={currentProductName} />)}
       </div>
     );
   }
-};
+}
 
 export default RelatedProducts;
+
+// var RelatedProducts = ({ relatedProductIds, currentProductFeatures, currentProductName }) => {
+
+//   if (relatedProductIds.length === 0) {
+//     return (
+//       <div className='related-products'>
+//       </div>
+//     );
+//   }
+
+//   var firstFourProductCards = relatedProductIds.slice(0, 4);
+//   var remainingProductCards = relatedProductIds.slice(4);
+//   console.log('firstFourProductCards:', firstFourProductCards);
+//   console.log('remainingProductCards:', remainingProductCards);
+
+//   return (
+//     <div className='related-products'>
+//       {firstFourProductCards.map(relatedProductId => <ProductCard isShown={true} isRelatedProduct={true} relatedProductId={relatedProductId} key={relatedProductId} currentProductFeatures={currentProductFeatures} currentProductName={currentProductName} />)}
+//       {remainingProductCards.map(relatedProductId => <ProductCard isShown={false} isRelatedProduct={true} relatedProductId={relatedProductId} key={relatedProductId} currentProductFeatures={currentProductFeatures} currentProductName={currentProductName} />)}
+//     </div>
+//   );
+
+// };
