efc2f33b1fcb0659898513c7c86bfc873543134c
fliszt2 <rkelly2012@gmail.com>
2021-03-08 18:27:20 -0500

    Implementing sale price rendering

---
+++ client/src/components/relatedItemsSubcomponents/ProductCardPrice.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import ajaxRequests from '../../../ajaxRequests.js';
+
+class ProductCardPrice extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      price: this.props.price,
+      salePrice: null,
+      relatedProductId: this.props.relatedProductId
+    };
+  }
+
+  componentDidMount() {
+    ajaxRequests.get('products/' + this.state.relatedProductId + '/styles', data => {
+      var updatedSalePrice = this.state.salePrice;
+      for (var obj of data.results) {
+        if (obj['sale_price']) {
+          console.log('sale price!:', obj['sale_price']);
+          updatedSalePrice = `Wow: $${obj['sale_price']}`;
+          break;
+        }
+      }
+      this.setState({ salePrice: updatedSalePrice });
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.price !== this.props.price) {
+      this.setState({ price: this.props.price });
+    }
+  }
+
+  render() {
+    if (this.state.salePrice) {
+      return(
+        <span className='product-card-text'>{this.state.salePrice}</span>
+      );
+    } else {
+      return(
+        <span className='product-card-text'>${this.state.price || '(price)'}</span>
+      );
+    }
+  }
+}
+
+export default ProductCardPrice;
